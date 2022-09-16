bf594516b2c751a6077f9ee7e72155db993024f9
Dariusz Depta <dariusz.depta@engos.software>
2022-09-16 13:48:07 +0200

    Refactoring.

--- coverage.sh
+++ coverage.sh
@@ -5,8 +5,10 @@ WORKING_DIRECTORY=$(pwd)
 # clean before proceeding
 cargo clean
 
+# set the last toolchain that profiles correctly (check from time to time if it's already better)
+export RUSTUP_TOOLCHAIN=nightly-2022-05-18
+
 # set instrumenting variables
-export RUSTUP_TOOLCHAIN=nightly-2022-05-18 # this is the last toolchain that correctly profiles libc
 export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
@@ -22,7 +24,7 @@ mkdir ./target/lcov
 mkdir ./target/coverage
 
 # generate coverage info
-grcov . --llvm -s . -t lcov --branch --ignore-not-existing --ignore "*cargo*" --ignore "*chrono-tz*" --ignore "*tests*" -o ./target/lcov/lcov.info
+grcov . --llvm -s . -t lcov --branch --ignore-not-existing --ignore "*.cargo*" --ignore "*tests*" -o ./target/lcov/lcov.info
 
 # generate coverage report
 genhtml -t "dec-number-sys" -q -o ./target/coverage ./target/lcov/lcov.info
